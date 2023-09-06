8f37d5f4e88345e7eca958963d7927f26219a127
oleksandr <dovhoteles.oleksandr@gmail.com>
2023-09-06 19:51:20 +0300

    added createActions() and createReducer()

--- RBCHK-School/Redux/Ivan Petrychenko/hero_admin_panel_template/src/actions/index.js
+++ RBCHK-School/Redux/Ivan Petrychenko/hero_admin_panel_template/src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
         request("http://localhost:3001/heroes")
@@ -12,24 +14,30 @@ export const fetchFilters = (request) => (dispatch) => {
             .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+export const heroesFetching = createAction('HEROES_FETCHING');
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+// export const heroesFetching = () => {
+//     return {
+//         type: 'HEROES_FETCHING'
+//     }
+// }
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroesFetched = createAction('HEROES_FETCHED');
+
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: 'HEROES_FETCHED',
+//         payload: heroes
+//     }
+// }
+
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
+
+// export const heroesFetchingError = () => {
+//     return {
+//         type: 'HEROES_FETCHING_ERROR'
+//     }
+// }
 
 export const filtersFetching = () => {
     return {
@@ -37,12 +45,14 @@ export const filtersFetching = () => {
     }
 }
 
+
 export const filtersFetched = (filters) => {
     return {
         type: 'FILTERS_FETCHED',
         payload: filters
     }
 }
+
 
 export const filtersFetchingError = () => {
     return {
@@ -57,25 +67,20 @@ export const activeFilterChanged = (filter) => {
     }
 }
 
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//                 type: 'ACTIVE_FILTER_CHANGED',
-//                 payload: filter
-//         })
-//     }, 1000)
+export const heroCreated = createAction('HERO_CREATED');
+
+// export const heroCreated = (hero) => {
+//     return {
+//         type: 'HERO_CREATED',
+//         payload: hero
+//     }
 // }
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
+export const heroDeleted = createAction('HERO_DELETED');
 
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
+// export const heroDeleted = (id) => {
+//     return {
+//         type: 'HERO_DELETED',
+//         payload: id
+//     }
+// }
